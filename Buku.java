b91553adc9baf69320a5535f521c7366b7ded019
yusuf302008 <myusufmustofa30@gmail.com>
2024-10-10 08:42:50 +0700

    Add files via upload

---
+++ Buku.java
@@ -0,0 +1,17 @@
+public class Buku {
+    int id;
+    String judul;
+    String pengarang;
+    int tahunTerbit;
+
+    //contructor parameter
+    public Buku(int id, String judul, String pengarang, int tahunTerbit){
+        //variabel = parameter
+        this.id = id;
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.tahunTerbit = tahunTerbit;
+
+
+    }  
+}
